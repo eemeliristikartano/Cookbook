9de2c7acb78ae9bbbfaf95219379c1dbbf9346bb
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2022-10-18 18:37:33 +0300

    Created first method for listing recipes

--- src/main/java/hh/swd20/Cookbook/web/FoodController.java
+++ src/main/java/hh/swd20/Cookbook/web/FoodController.java
@@ -1,5 +1,25 @@
 package hh.swd20.Cookbook.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.swd20.Cookbook.domain.FoodRepository;
+
+@Controller
 public class FoodController {
+	
+	@Autowired
+	private FoodRepository frepository;
+	
+	/*
+	 *Index endpoint contains list of recipes from DB. It is visible to everyone.
+	 */
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("recipes", frepository.findAll());
+		return "index";
+	}
 
 }
