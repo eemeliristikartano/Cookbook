1b72932dc90955d0eef8ca7996b5798a074d4d77
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2022-10-20 15:56:03 +0300

    Added method that finds recipe by id

--- src/main/java/hh/swd20/Cookbook/web/FoodController.java
+++ src/main/java/hh/swd20/Cookbook/web/FoodController.java
@@ -1,9 +1,13 @@
 package hh.swd20.Cookbook.web;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import hh.swd20.Cookbook.domain.FoodRepository;
 
@@ -18,8 +22,19 @@ public class FoodController {
 	 */
 	@GetMapping("/")
 	public String index(Model model) {
-		model.addAttribute("recipes", frepository.findAll());
+		model.addAttribute("foods", frepository.findAll());
 		return "index";
+	}
+	
+	/*
+	 * Finds recipe by id from DB and lists name, ingredients, instructions,
+	 * date created and username who published recipe. Visible to everyone.
+	 */
+	
+	@GetMapping("/recipe/{id}")
+	public String recipe(@PathVariable("id")Long id, Model model) {
+		model.addAttribute("food", frepository.findById(id).get());
+		return "recipe";
 	}
 
 }
