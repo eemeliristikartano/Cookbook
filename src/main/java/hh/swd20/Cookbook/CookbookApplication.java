14c69c676f9b8ddb92f911a8435fc7df6c093c36
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2022-11-04 20:13:26 +0200

    -

--- src/main/java/hh/swd20/Cookbook/CookbookApplication.java
+++ src/main/java/hh/swd20/Cookbook/CookbookApplication.java
@@ -19,34 +19,34 @@ public class CookbookApplication {
 		SpringApplication.run(CookbookApplication.class, args);
 	}
 	
-//	@Bean
-//	public CommandLineRunner cookbookDemo(CategoryRepository crepository, UnitRepository unrepository) {
-//		return (args) -> {
-//			
-//			Unit unit = new Unit("");
-//			Unit unit1 = new Unit("tl");
-//			Unit unit2 = new Unit("rkl");
-//			Unit unit3 = new Unit("mm");
-//			Unit unit4 = new Unit("dl");
-//			Unit unit5 = new Unit("l");
-//			Unit unit6 = new Unit("g");
-//			Unit unit7 = new Unit("kg");
-//			Unit unit8 = new Unit("prk");
-//			Unit unit9 = new Unit("pkt");
-//			Unit unit10 = new Unit("rs");
-//			Unit unit11 = new Unit("tlk");
-//			List<Unit> units = List.of(unit, unit1, unit2, unit3, unit4, unit5, unit6, unit7, unit8, unit9, unit10, unit11);
-//			unrepository.saveAll(units);
-//	
-//			Category category = new Category("");
-//			Category category1 = new Category("Seka");
-//			Category category2 = new Category("Vegaani");
-//			Category category3 = new Category("Kasvis");
-//			List<Category> categorys = List.of(category, category1, category2, category3);
-//			crepository.saveAll(categorys);
-//
-//		};
-//	}
+	@Bean
+	public CommandLineRunner cookbookDemo(CategoryRepository crepository, UnitRepository unrepository) {
+		return (args) -> {
+			
+			Unit unit = new Unit("");
+			Unit unit1 = new Unit("tl");
+			Unit unit2 = new Unit("rkl");
+			Unit unit3 = new Unit("mm");
+			Unit unit4 = new Unit("dl");
+			Unit unit5 = new Unit("l");
+			Unit unit6 = new Unit("g");
+			Unit unit7 = new Unit("kg");
+			Unit unit8 = new Unit("prk");
+			Unit unit9 = new Unit("pkt");
+			Unit unit10 = new Unit("rs");
+			Unit unit11 = new Unit("tlk");
+			List<Unit> units = List.of(unit, unit1, unit2, unit3, unit4, unit5, unit6, unit7, unit8, unit9, unit10, unit11);
+			unrepository.saveAll(units);
+	
+			Category category = new Category("");
+			Category category1 = new Category("Seka");
+			Category category2 = new Category("Vegaani");
+			Category category3 = new Category("Kasvis");
+			List<Category> categorys = List.of(category, category1, category2, category3);
+			crepository.saveAll(categorys);
+
+		};
+	}
 	
 	
 
